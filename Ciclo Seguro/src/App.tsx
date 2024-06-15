5ddf04809c5434bc5e4af8d196d89cfded01e971
Larissa Oliveira <larissa.2359@gmail.com>
2024-06-15 17:28:08 -0300

    initial setup

---
+++ Ciclo Seguro/src/App.tsx
@@ -0,0 +1,16 @@
+import { Suspense } from 'react';
+import { useRoutes } from 'react-router-dom';
+import routes from './routes';
+
+
+const App = () => {
+
+    const elements = useRoutes(routes);
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            {elements}
+        </Suspense>
+    );
+};
+
+export default App;
