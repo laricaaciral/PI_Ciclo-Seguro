16e61ed61d3a8859c89356ba9f352e5d73d6d6c6
Larissa Oliveira <larissa.2359@gmail.com>
2024-06-16 01:51:14 -0300

    adding route to app

--- Ciclo Seguro/src/App.tsx
+++ Ciclo Seguro/src/App.tsx
@@ -1,15 +1,23 @@
 import { Suspense } from 'react';
 import { useRoutes } from 'react-router-dom';
 import routes from './routes';
+import Navbar from './components/Navbar/Navbar';
+import "./styles/App.css";
+import Footer from './components/Footer/Footer';
 
 
 const App = () => {
 
     const elements = useRoutes(routes);
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+            <Suspense fallback={<div>Loading...</div>}>
+            <Navbar/>
             {elements}
+            <Footer hasFooterName={true}/>
         </Suspense>
+        </div>
+        
     );
 };
 
